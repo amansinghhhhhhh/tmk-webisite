935d82b52312b759a3d87f19556c979d2278790d
Parth <parth.a2digi@gmail.com>
2026-09-05 13:12:43 +0530

    deploy: 2b73d9c

--- assets/BlogDetail-DkLsFhWS.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./react-vendor-B6DOvCGh.js";import{t}from"./PostDetail-7TkkQPcz.js";var n=e();function r(){return(0,n.jsx)(t,{categoryId:4,backLink:`/blogs/`,backLabel:`Back to Blog`,listLink:`/blogs/`,listLabel:`View All Blogs`,routePrefix:`blog`})}export{r as default};
